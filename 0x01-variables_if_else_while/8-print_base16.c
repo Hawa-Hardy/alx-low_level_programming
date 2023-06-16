61fe9a112b85f498946ffd97876a618bc66ba4dc
Hawa-Hardy <hawahardy@gmail.com>
2023-06-16 18:22:02 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
