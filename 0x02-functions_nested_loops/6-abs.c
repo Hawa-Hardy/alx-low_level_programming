76e172c7868fa5748967d2b053693368bd43d393
Hawa-Hardy <hawahardy@gmail.com>
2023-06-20 17:22:08 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of an integer
+ * @num: the integer being checked
+ * Return: the absolute value of int
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	return (-num);
+}
