6ecd7d22d2273281070e54dff710646127e05fae
Hawa-Hardy <hawahardy@gmail.com>
2023-06-26 18:12:29 +0000

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
  * print_rev - prints a string, in reverse, followed by a new line
  *
  * @s: string
- * return: 0
+ * return: Always 0 (Success)
  */
 void print_rev(char *s)
 {
@@ -18,7 +18,7 @@ void print_rev(char *s)
 	}
 	s--;
 
-	for (count = length - 1; count > 0; count--)
+	for (count = length; count > 0; count--)
 	{
 		_putchar(*s);
 		s--;
