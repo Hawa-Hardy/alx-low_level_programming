5c815a7a16251d9705d8e869362e7afab47e4efd
Hawa-Hardy <hawahardy@gmail.com>
2023-06-29 21:55:35 +0000

    Task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,28 +3,28 @@
 
 /**
  * rot13 - Encoder for ROT13 cipher
- * @str: Pointer to the string to be encoded
+ * @s: Pointer to the string to be encoded
  *
  * Return: Pointer to the encoded string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int index1;
 	int index2;
-	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (index1 = 0; str[index1] != '\0'; index1++)
+	for (index1 = 0; s[index1] != '\0'; index1++)
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (s[index1] == data1[index2])
 			{
-				str[index1] = rot13[index2];
+				s[index1] = datarot[index2];
 				break;
 			}
 		}
 	}
-
-	return (str);
+	return (s);
 }
+
