a1b2e8c32bab1691e67bc57a0668428c1f54a29e
Hawa-Hardy <hawahardy@gmail.com>
2023-09-06 00:21:26 +0000

    file cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ char *create_buffer(char *file)
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, r, w;
+	int from, file_to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -46,15 +46,15 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	file_from = open(argv[1], O_RDONLY);
-	r = read(file_from, buffer, 1024);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (file_from == -1 || r == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read file_from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -68,13 +68,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		r = read(file_from, buffer, 1024);
+		r = read(from, buffer, 1024);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
-	close_file(file_from);
+	close_file(from);
 	close_file(file_to);
 
 	return (0);
@@ -86,11 +86,11 @@ int main(int argc, char *argv[])
  */
 void close_file(int fd)
 {
-	int c;
+	int x;
 
-	c = close(fd);
+	x = close(fd);
 
-	if (c == -1)
+	if (x == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
