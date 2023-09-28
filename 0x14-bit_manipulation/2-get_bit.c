c26d638411adecd3a40d56c85c0419983d5621fc
Hawa Hardy <76821645+Hawa-Hardy@users.noreply.github.com>
2023-09-28 18:17:09 +0000

    ALX  0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to search
+ * @index: index, starting from 0 of the bit
+ *
+ * Return: value of the bit index
+ * or -1 if error occurs
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_value;
+
+	if (index > 63)
+		return (-1);
+
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
+}
