c2a51c089c0571cad24c41f6e71fbb2c5c6931b3
Hawa-Hardy <hawahardy@gmail.com>
2024-02-02 21:30:23 +0000

    libtest

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://raw.github.com/Hawa-Hardy/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libtest.so
+wget -P https://github.com/Hawa-Hardy/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD ="$PWD/../libtest.so"
